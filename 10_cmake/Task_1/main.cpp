0efbd9a69e0fdff211c9363840e646fd73d9d951
Yoda <yoda@example.com>
2024-12-10 09:26:43 +0300

    complete task_1 in cmake

---
+++ 10_cmake/Task_1/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    system("chcp 1251");
+
+    string name{};
+
+    cout << "Введите имя: ";
+    cin >> name;
+
+    cout << "Здравствуйте, " << name << "!" << endl;
+
+    return 0;
+}
